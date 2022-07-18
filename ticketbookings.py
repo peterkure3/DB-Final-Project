81e6ad58992834330900880accf06d7dbf18e5d2
peterkure3 <peterkure256@gmail.com>
2022-07-18 10:29:53 +0300

    Final touches

--- ticketbookings.py
+++ ticketbookings.py
@@ -6,7 +6,7 @@ open("ticketbookings.db", "w").close()
 
 db = SQL("sqlite:///ticketbookings.db")
 
-db.execute("CREATE TABLE customers(id INTEGER, name TEXT, email TEXT, accessnumber INTEGER, PRIMARY KEY(id))")
+db.execute("CREATE TABLE customers(id INTEGER, name TEXT, email TEXT, accessnumber INTEGER, phone TEXT, PRIMARY KEY(id))")
 
 db.execute("CREATE TABLE tickets(ticket_id INTEGER, name TEXT, viewer TEXT, FOREIGN KEY(ticket_id) REFERENCES customers(id))")
 
@@ -19,21 +19,22 @@ with open("ticketbookings.csv","r") as file:
         name = row["Name"].upper()
         accessnumber=row["Access Number"]
         email=row["Email"]
+        phone=row["Phone Number"]
 
-        id=db.execute("INSERT INTO customers(name,email,accessnumber) VALUES(?,?,?)",name,email,accessnumber)
+        id=db.execute("INSERT INTO customers(name,email,accessnumber,phone) VALUES(?,?,?,?)",name,email,accessnumber,phone)
         
         
         ticket_id=id
         name = row["Name"].upper()
-        viewer=row["Viewing"]
-        ticket_id=db.execute("INSERT INTO tickets(ticket_id,name,viewer) VALUES(?,?,?)",ticket_id,name,viewer)
+        view=row["Viewing"]
+        ticket_id=db.execute("INSERT INTO tickets(ticket_id,name,viewer) VALUES(?,?,?)",ticket_id,name,view)
 
         
         movie=row["Movie"]
         movie_id=id
         movie_number=row["MovieID"]
         viewer=row["Viewing"]
-        id = db.execute("INSERT INTO movies(movie_id,movie, movie_number, viewer) VALUES(?,?,?,?)",movie_id,movie, movie_number,viewer)
+        id = db.execute("INSERT INTO movies(movie_id,movie, movie_number, viewer) VALUES(?,?,?,?)",movie_id,movie, movie_number,view)
         
 
 
